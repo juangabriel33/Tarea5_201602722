fb9b1611c40bf0fa476ce8d6bd0e4adedf2eef46
juangabriel33 <99620998+juangabriel33@users.noreply.github.com>
2022-02-26 22:34:40 -0600

    Add files via upload

---
+++ Tarea5/Tarea5/Form1.vb
@@ -0,0 +1,33 @@
+﻿Public Class Form1
+    Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
+        Dim n, i, fac, acum As Integer
+
+
+        Try
+            n = Val(tbxNum.Text)
+            acum = 0
+
+            If (n > 0) Then
+                fac = 1
+                For i = 1 To n Step 1
+                    fac = fac * i
+                Next
+                tbxFact.Text = (fac.ToString)
+                For i = 0 To n Step 1
+                    lvw.Items.Add(i)
+                    If (i Mod 4 = 0) Then
+                        acum = acum + 1
+                    End If
+                Next
+                tbxMult.Text = (acum.ToString)
+            Else
+                MsgBox("El número ingresado debe ser positivo", MsgBoxStyle.Information, "ADVERTENCIA")
+            End If
+        Catch ex As Exception
+
+        End Try
+
+
+
+    End Sub
+End Class
